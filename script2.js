24339c5558009e7382d671e6751170ce17895ba5
Aedan4096 <aedanszchen@gmail.com>
2026-07-31 13:23:09 -0400

    Purchasing options

--- script2.js
+++ script2.js
@@ -1,5 +1,15 @@
+const filter = document.querySelector(".filter");
+const filterContent = document.querySelector(".filter-content");
 const toggleBtn = document.getElementById("theme-toggle");
 const themeSheet = document.getElementById("theme-sheet");
+
+function openFilter() {
+    if (filterContent.style.display === "none") {
+      filterContent.style.display = "block";
+    } else {
+      filterContent.style.display = "none";
+    }
+  };
 
 function themeToggle() {
 if (themeSheet.getAttribute("href") === "style.css") {
@@ -9,4 +19,6 @@ if (themeSheet.getAttribute("href") === "style.css") {
     }
 };
 
+
+filter.addEventListener("click", openFilter);
 toggleBtn.addEventListener("click", themeToggle);
